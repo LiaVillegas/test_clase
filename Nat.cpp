2e4333430c4bc3c74600468aae349b9907ee5110
LiaVillegas <liavillegasj@gmail.com>
2018-08-08 21:32:06 -0500

    First commit
    First commit in class

---
+++ Nat.cpp
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+      
+  std::string name= "Nat";
+  std::string surname= "Villegas";
+  std::string fullname;
+  fullname = name + " " +surname;
+  std::cout << "Your name is: "<< fullname << std::endl;
+  return  0;
+}
